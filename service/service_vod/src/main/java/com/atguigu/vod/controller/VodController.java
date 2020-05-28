fbcf8c0ef54912b0c50f9b599c86821a438fd518
abc123-code <1373522290@qq.com>
2020-05-28 10:57:40 +0800

    fix #阿里云视频删除接口开发

--- service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/atguigu/vod/controller/VodController.java
@@ -1,13 +1,15 @@
 package com.atguigu.vod.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.DeleteVideoRequest;
 import com.atguigu.common.utils.R;
+import com.atguigu.serviceBase.exceptionhandler.GuliException;
 import com.atguigu.vod.service.VodService;
+import com.atguigu.vod.utils.ConstantVodUtils;
+import com.atguigu.vod.utils.InitVodClient;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -24,6 +26,26 @@ public class VodController {
         //返回上传视频id
         String videoId=vodService.uploadVideoAliyun(file);
         return R.ok().data("videoId",videoId);
+    }
+
+    //根据视频id删除阿里云视频
+    @DeleteMapping("removeAliyunVideo/{id}")
+    public R removeAliyunVideo(@PathVariable String id){
+        try {
+            //初始化对象
+            DefaultAcsClient client = InitVodClient.initVodClient(ConstantVodUtils.ACCESS_KEY_ID, ConstantVodUtils.ACCESS_KEY_SECRET);
+            //创建一个删除视频的request对象
+            DeleteVideoRequest request = new DeleteVideoRequest();
+            //向request设置视频id
+            request.setVideoIds(id);
+            //调用初始化对象的方法实现删除
+            client.getAcsResponse(request);
+            return R.ok();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new GuliException(20001,"删除视频失败");
+        }
+
     }
 
 }
